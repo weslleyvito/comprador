c430047f08d640e63b833541dddef7aa2b31918d
robinhue <rpfobo32@gmail.com>
2023-06-17 18:01:23 -0300

    Criação de componentes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:comparador/controller.dart';
+import 'package:comparador/components/input-generics.dart';
+import 'package:comparador/controller/controller.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -33,8 +34,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-    final Controlador _controlador = Get.find<Controlador>();
+  final Controlador _controlador = Get.find<Controlador>();
 
   // Função que calcula o preço por quilo de um produto
   precoPorQuilo(var peso, var preco) {
@@ -42,17 +42,20 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   // Função que compara dois produtos e retorna qual é mais barato
-   compararProdutos(
-      var peso1, var preco1, var peso2, var preco2) {
+  compararProdutos(var peso1, var preco1, var peso2, var preco2) {
     var ppq1 = precoPorQuilo(peso1, preco1); // Preço por quilo do produto 1
     var ppq2 = precoPorQuilo(peso2, preco2); // Preço por quilo do produto 2
     if (ppq1 < ppq2) {
-      _controlador.mensagem.value = "O produto 1 está mais em conta, pois custa R\$ $ppq1 por quilo.";
+      _controlador.mensagem.value =
+          "O produto 1 está mais em conta, pois custa R\$ $ppq1 por quilo.";
       return _controlador.mensagem.value;
     } else if (ppq1 > ppq2) {
-      _controlador.mensagem.value = "O produto 2 está mais em conta, pois custa R\$ $ppq2 por quilo.";
+      _controlador.mensagem.value =
+          "O produto 2 está mais em conta, pois custa R\$ $ppq2 por quilo.";
       return _controlador.mensagem.value;
-    } else {_controlador.mensagem.value = "Os dois produtos têm o mesmo preço por quilo: R\$ $ppq1.";
+    } else {
+      _controlador.mensagem.value =
+          "Os dois produtos têm o mesmo preço por quilo: R\$ $ppq1.";
       return _controlador.mensagem.value;
     }
   }
@@ -64,7 +67,6 @@ class _MyHomePageState extends State<MyHomePage> {
     TextEditingController preco1 = TextEditingController();
     TextEditingController preco2 = TextEditingController();
 
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Comparador de Produtos'),
@@ -75,35 +77,62 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TextFormField(
+              InputGeneric(
+                label: 'Peso 1 (kg)',
                 controller: peso1,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(labelText: 'Peso 1 (kg)'),
-
+                typeInput: TextInputType.number,
+                validator: (String? value) {
+                  if (value!.isEmpty || value == null) {
+                    return "Campo Obrigatorio";
+                  }
+                  return null;
+                },
+                errorText: "Peso não informado!",
               ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(labelText: 'Preço 1 (R\$)'),
+              InputGeneric(
+                label: 'Preço 1 (R\$)',
                 controller: preco1,
-
+                typeInput: TextInputType.number,
+                validator: (String? value) {
+                  if (value!.isEmpty || value == null) {
+                    return "Campo Obrigatorio";
+                  }
+                  return null;
+                },
+                errorText: "Peso não informado!",
               ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(labelText: 'Peso 2 (kg)'),
+              InputGeneric(
+                label: 'Peso 2 (kg)',
                 controller: peso2,
-
+                typeInput: TextInputType.number,
+                validator: (String? value) {
+                  if (value!.isEmpty || value == null) {
+                    return "Campo Obrigatorio";
+                  }
+                  return null;
+                },
+                errorText: "Peso não informado!",
               ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(labelText: 'Preço 2 (R\$)'),
+              InputGeneric(
+                label: 'Preço 2 (R\$)',
                 controller: preco2,
-
+                typeInput: TextInputType.number,
+                validator: (String? value) {
+                  if (value!.isEmpty || value == null) {
+                    return "Campo Obrigatorio";
+                  }
+                  return null;
+                },
+                errorText: "Peso não informado!",
               ),
-              SizedBox(height: 16.0),
+              const SizedBox(height: 16.0),
               ElevatedButton(
                 onPressed: () {
-                
-                      compararProdutos(double.parse(peso1.text), double.parse(preco1.text), double.parse(peso2.text), double.parse(preco2.text));
+                  compararProdutos(
+                      double.parse(peso1.text),
+                      double.parse(preco1.text),
+                      double.parse(peso2.text),
+                      double.parse(preco2.text));
                 },
                 child: Text('Comparar'),
               ),
