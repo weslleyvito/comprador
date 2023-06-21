c1e42b8745a114ab95c916fbb35a1dd2b3072f62
robinhue <rpfobo32@gmail.com>
2023-06-20 22:10:28 -0300

    Criação de comentários nos campos de button e input

--- lib/components/button-generic.dart
+++ lib/components/button-generic.dart
@@ -1,21 +1,31 @@
+// Importando os pacotes necessários para usar os widgets do flutter
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+//Definindo uma classe ButtonGeneric do tipo Stateful que pode mudar ao longo do tempo
 class ButtonGeneric extends StatefulWidget {
+  //Contrutor recebe o parâmetro key para identificar um widget de forma única
   const ButtonGeneric({Key? key}) : super(key: key);
 
+  //Propriedade onPressed vazia, usada para definir uma função quando o botão for pressionado
   get onPressed => null;
 
+  //Definindo o método createState que retorna _ButtonGeneric
   @override
   State<ButtonGeneric> createState() => _ButtonGenericState();
 }
 
+//Definindo uma classe do chamada _ButtonGenericState que é uma subclasse de ButtonGeneric
 class _ButtonGenericState extends State<ButtonGeneric> {
   @override
+  //Método build que recebe o parâmetro context que é usado para acessarinformações sobre o ambiente do widget
   Widget build(BuildContext context) {
+    //Método build retorna um widget do tipo ElevatedButton
     return ElevatedButton(
+      //Parâmetro onPressed que recebe o valor do widget passado no ButtonGeneric, que é nulo
       onPressed: widget.onPressed,
+      //Parâmetro child que recebe o valor null
       child: null,
     );
   }
--- lib/components/input-generics.dart
+++ lib/components/input-generics.dart
@@ -1,9 +1,13 @@
+// Importando os pacotes necessários para usar os widgets do flutter
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 // ignore: must_be_immutable
+//Definindo uma classe InputGeneric do tipo Stateful que pode mudar ao longo do tempo
+
 class InputGeneric extends StatefulWidget {
+  //Declara os atributos da classe que serão incializados mais tarde com o late
   late String label;
   late TextEditingController controller;
   late TextInputType typeInput;
@@ -11,6 +15,7 @@ class InputGeneric extends StatefulWidget {
   late String Function(String?) onChange;
   late String errorText;
 
+  // Definindo o construtor da classe, recebendo os atributos como parâmetros, required mostra que o campo é obrigatório
   InputGeneric({
     Key? key,
     required this.label,
@@ -21,25 +26,33 @@ class InputGeneric extends StatefulWidget {
     // required this.errorText,
   }) : super(key: key);
 
+  //Definindo o método createState que retorna _InputGenericState
   @override
   State<InputGeneric> createState() => _InputGenericState();
 }
 
+//Definindo uma classe do chamada _InputGenericState que é uma subclasse de InputGeneric
 class _InputGenericState extends State<InputGeneric> {
+  //Definindo o método quando o widget inserido pela primeira vez na árvore de widgets
   @override
   void initState() {
-    super.initState();
-    widget.controller = TextEditingController();
+    super.initState(); //Chama o método da superclasse
+    widget.controller =
+        TextEditingController(); // Inicializa o controlador do widget com um novo objeto TextEditingController
   }
 
+  //Definindo o método quando o widget inserido pela primeira vez na árvore de widgets
   @override
   void dispose() {
-    widget.controller.dispose();
-    super.dispose();
+    widget.controller
+        .dispose(); // Descarta o controlador do widget para liberar recursos e evitar vazamentos de memória
+    super.dispose(); // Chama o método da superclasse
   }
 
+  // Definindo o método que é chamado quando o usuário termina de editar o texto no campo de entrada
   void _validateInput(String value) {
     setState(() {
+      // Atualiza o estado do widget com a nova informação
       widget.errorText = widget.validator!(value) ?? '';
     });
   }
